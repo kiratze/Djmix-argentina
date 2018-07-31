114325e131f8cac133730428b4ecbc47534112a4
kiratze <nancy.a.viqueira@gmail.com>
2018-07-31 18:13:57 -0300

    Initial commit

---
+++ js/galeria.js
@@ -0,0 +1,14 @@
+// $('.gal-img').click(function(e){
+//   var img = e.target.src;
+//   $('body').append('<div class="mod" id="mod"><img src="'+ img +'" class="mod-img"><a href="'+ img +'" class="fullsize animated bounceInDown" style="text-decoration:none;">Click para ver tamaño real</a><div class="mod-btn" id="mod-btn"><i class="fas fa-times"></i></div></div>');
+//   $('#mod-btn').click(function(){
+//     $('#mod').remove();
+//   })
+//  })
+
+
+// $(document).keyup(function(e){
+// 	if (e.which==27) {
+// 	$('#mod').remove();	
+// 	}
+// })
